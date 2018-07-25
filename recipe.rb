6dac6aa206b40bfc01e24048c8c56b57005217b2
madeline57 <maddiejobeth@aol.com>
2018-07-25 18:13:54 +0000

    automatically backed up by learn

---
+++ recipe.rb
@@ -0,0 +1,4 @@
+ingredents=["almond flour","gluten freewhole oats","kosher salt","baking powder","baking soda","xanthan gum","sliverd almonds,lightly toasted","mini dark chocolate chips","olive oil","agave"]
+
+me=["2 cups", "1 cup","1/2 tsp.","1/2 tsp.", "1/4 tsp.","1/2 tsp.","1/4 cups","3/4 cups","1/2 cups","1/4 cups"] 
+
